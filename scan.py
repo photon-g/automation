6bf045407cf1ee655419224c16da73a7eebe3e8d
Robert King <robert.king@lockeandpartners.com>
2023-06-21 23:38:59 +0200

    research x

--- scan.py
+++ scan.py
@@ -4,6 +4,7 @@ import sys
 #import logging
 import subprocess
 import os
+from slack_notifications import Slack
 
 SUCCESS='{} has been tested succesfully\n please re-synchronize your clone with the dev branch'
 FAILED='{} tests have failed\n please analyse the generated report'
@@ -22,18 +23,21 @@ def log_error_n_abort(msg:str) -> None:
     print(str)
     #sys.exit(1) 
 
+
 if __name__ == '__main__':
 
+    slack = Slack(os.environ["STOK"])
     current_branch = subprocess.getoutput("git branch --show-current")
     commit_msg = subprocess.getoutput("git log -1 --pretty=%B")
 
     print(current_branch)
     print(commit_msg)
+
     if not analyzes(current_branch):
+        slack.send_notify('#photon-repo', username='PTBOT', text=FAILED.format('EM-X'))        
         os.environ['SMSG']='the name of the branch is not correct'
         os.environ['BUFFER']='the name of the branch is not correct'
         print(os.environ['BUFFER']) 
         log_error_n_abort('the name of the branch is not correct')
     else: 
         os.environ['SMSG']=current_branch
-
