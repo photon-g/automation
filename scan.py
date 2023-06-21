755da8c022c63634da441f326307b0a7c00ecadc
Robert King <robert.king@lockeandpartners.com>
2023-06-21 06:02:01 +0200

    add func

--- scan.py
+++ scan.py
@@ -4,6 +4,11 @@ import sys
 #import logging
 import subprocess
 
+def analyzes(msg:str,tokens:list[str]=["EM","dev"]) -> bool:
+    if msg in tokens:
+        return True
+    return False
+
 def analyze(msg:str,token:str="EM") -> bool:
     if token in msg:
         return True
@@ -18,8 +23,8 @@ if __name__ == '__main__':
     current_branch = subprocess.getoutput("git branch --show-current")
     commit_msg = subprocess.getoutput("git log -1 --pretty=%B")
 
-    if not analyze(current_branch): 
+    if not analyzes(current_branch): 
         log_error_n_abort('the name of the branch is not correct')
 
-    if not analyze(commit_msg):
+    if not analyzes(commit_msg):
         log_error_n_abort('the commit message is not correct')
