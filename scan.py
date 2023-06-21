30f67e703ed8345fdcec9fbd4157e68fa3383fec
Robert King <robert.king@lockeandpartners.com>
2023-06-21 05:48:14 +0200

    EM-X: fix action

--- scan.py
+++ scan.py
@@ -1,7 +1,8 @@
 
-import os
+
 import sys
 #import logging
+import subprocess
 
 def analyze(msg:str,token:str="EM") -> bool:
     if token in msg:
@@ -14,12 +15,11 @@ def log_error_n_abort(msg:str) -> None:
 
 if __name__ == '__main__':
 
-    branch_name=os.environ['GIT_B']
-    commit_msg=os.environ['GIT_C']
-    
-    if not analyze(branch_name): 
+    current_branch = subprocess.getoutput("git branch --show-current")
+    commit_msg = subprocess.getoutput("git log -1 --pretty=%B")
+
+    if not analyze(current_branch): 
         log_error_n_abort('the name of the branch is not correct')
 
     if not analyze(commit_msg):
         log_error_n_abort('the commit message is not correct')
-
