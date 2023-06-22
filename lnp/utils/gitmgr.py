0c5bd6152082ff6cfcb7b5f275f1dbb17a6816ba
Robert King <robert.king@lockeandpartners.com>
2023-06-22 03:05:26 +0200

    fix workflow

--- lnp/utils/gitmgr.py
+++ lnp/utils/gitmgr.py
@@ -5,8 +5,8 @@ from lnp.core.file_handler import FileHandler as fl
 class GitMgr:
     def __init__(self,path_conf:str):
         self.__conf=fl(path_conf).read()
-        self.__branch_item = gi(self.conf["branch"],"cmd","success","failure")
-        self.__msg_item = gi(self.conf["msg"],"cmd","success","failure")
+        self.__branch_item = gi(self.__conf["branch"],"cmd","success","failure")
+        self.__msg_item = gi(self.__conf["msg"],"cmd","success","failure")
 
     def current_branch(self) -> gi:
         return self.__branch_item
