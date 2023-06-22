a66188908992cdb52ea211b5301b49fbc6cde3ea
Robert King <robert.king@lockeandpartners.com>
2023-06-22 03:48:43 +0200

    fix comment

--- scan.py
+++ scan.py
@@ -6,7 +6,7 @@ from lnp.utils.notifier import Notifier
 from lnp.utils.gitmgr import GitMgr
 from lnp.core.file_handler import FileHandler as fl
 
-#todo wrap this in a conf file
+#todo wrap this in a json file
 CHAN='#photon-repo'
 BOT='PTBOT'
 GIT_ARTIFACTS_CONF="conf/gititem.json"
