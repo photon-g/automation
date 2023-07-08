b08ff934beae53332cd101c5377084db8b5c6d31
Robert King <ro.king.eu@gmail.com>
2023-07-09 01:16:04 +0200

    EM-88: enhancement

--- scan.py
+++ scan.py
@@ -24,26 +24,32 @@ formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(messag
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
+#todo allows to pass x args
+def execute_predicat(_handler,_arg):
+    if not _handler(_arg):
+        sys.exit(1)
+
+#todo allows to pass x args
+def execute_predicat_with_cond(_cond,_handler,_arg):
+    if _cond:
+        _handler(_arg)
+        sys.exit(1)
+
 if __name__ == "__main__":
     l_notifier = Notifier(CHAN, BOT, os.environ["STK"])
     l_git_mgr = GitMgr(GIT_ARTIFACTS_CONF)
-    
-    if not l_notifier.send_msg(l_git_mgr.current_branch()):
-        sys.exit(1)
-
-    if not l_notifier.send_msg(l_git_mgr.commit_msg()):
-        sys.exit(1)
+   
+    execute_predicat(l_notifier.send_msg,l_git_mgr.current_branch())
+    execute_predicat(l_notifier.send_msg,l_git_mgr.commit_msg())
 
     l_branch = l_git_mgr.current_branch()
     l_commit = l_git_mgr.commit_msg()
     l_author = l_git_mgr.author()
 
-    if l_branch.get_id() != l_commit.get_id():
-        l_notifier.send_raw_msg(
-            "the branch {} & \n the commit {} \n do not point to the same JIRA ticket".format(
-                l_branch.get_output(), l_commit.get_output()
-            )
-        )
-        sys.exit(1)
+    execute_predicat_with_cond(l_branch.get_id() != l_commit.get_id(),
+                               l_notifier.send_raw_msg,
+                               "the branch {} & \n the commit {} \n do not point to the same JIRA ticket".format(
+                               l_branch.get_output(), l_commit.get_output())
+    )
 
     l_notifier.send_raw_msg("\n {} has been pushed {} succesfully".format(l_author.get_output(),l_branch.get_output()))
