2ddbe82ee5b36556ad1f745a51a4e182e2fec6d0
Robert King <robert.king@lockeandpartners.com>
2023-09-08 00:20:43 +0200

    EM-109: remove ac cleaning

--- remove.py
+++ remove.py
@@ -1,12 +1,9 @@
 
 import os
-import requests
 
 current_ref = os.environ["CBRANCH"]
 token = os.environ["GITHUB_TOKEN"]
 git_repo = os.environ["GITHUB_REPOSITORY"]
-
-#headers = {'Authorization': 'token ' + token}
 
 #curl method
 api_method=f"https://api.github.com/repos/{git_repo}/git/{current_ref}"
@@ -17,7 +14,3 @@ h_op_3="X-GitHub-Api-Version: 2022-11-28"
 l_curl_cmd=f'curl -L -X DELETE -H "{h_op_1}" -H "{h_op_2}" -H "{h_op_3}"'+" "+api_method 
 print(l_curl_cmd)
 os.system(l_curl_cmd)
-
-#requests method
-#login = requests.delete('https://api.github.com/' + 'repos/' + user + '/' + repo, headers=headers)
-print("launching dummy test ......")
